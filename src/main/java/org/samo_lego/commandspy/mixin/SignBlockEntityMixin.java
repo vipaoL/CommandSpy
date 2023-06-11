091ae28295b33e2986690bceca4f4294e87092b2
vipaol <vipaolmail@gmail.com>
2023-06-11 15:48:30 +0400

    WIP

--- src/main/java/org/samo_lego/commandspy/mixin/SignBlockEntityMixin.java
+++ src/main/java/org/samo_lego/commandspy/mixin/SignBlockEntityMixin.java
@@ -1,9 +1,11 @@
 package org.samo_lego.commandspy.mixin;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
 import org.apache.logging.log4j.core.lookup.StrSubstitutor;
 import org.samo_lego.commandspy.CommandSpy;
@@ -25,12 +27,12 @@ public class SignBlockEntityMixin {
             method = "executeClickCommands",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/commands/Commands;performPrefixedCommand(Lnet/minecraft/commands/CommandSourceStack;Ljava/lang/String;)I"
+                    target = "Lnet/minecraft/commands/Commands;executeClickCommandsIfPresent(Lnet/minecraft/commands/CommandSourceStack;Ljava/lang/String;)I"
             ),
             locals = LocalCapture.CAPTURE_FAILHARD
 
     )
-    private void catchSignCommand(ServerPlayer player, CallbackInfoReturnable<Boolean> cir, Component[] texts, int i, int j, Component text, Style style,  ClickEvent clickEvent) {
+    private void catchSignCommand(ServerPlayer player, Level level, BlockPos blockPos, CallbackInfoReturnable<Boolean> cir, Component[] texts, int i, int j, Component text, Style style,  ClickEvent clickEvent) {
         if(config.logging.logSignCommands) {
             SignBlockEntity sign = (SignBlockEntity) (Object) this;
 
@@ -53,7 +55,7 @@ public class SignBlockEntityMixin {
             StrSubstitutor sub = new StrSubstitutor(valuesMap);
 
             // Logging to console
-            //CommandSpy.logCommand(sub.replace(message), sign.createCommandSourceStack((ServerPlayer) player), MODID + ".log.signs");
+            CommandSpy.logCommand(sub.replace(message), sign.createCommandSourceStack(player, level, blockPos), MODID + ".log.signs");
         }
     }
 }
